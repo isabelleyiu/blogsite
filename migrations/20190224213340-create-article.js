997591e7c7e622999efd70461f8914d1f3bccfc7
Isabelle Yiu <isabelle.yiu.jobs@gmail.com>
2019-02-24 18:21:06 -0800

    Add migrations folder

---
+++ migrations/20190224213340-create-article.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Articles', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      author: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [1]
+        }
+      },
+      content: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+        len: [1]
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Articles');
+  }
+};
