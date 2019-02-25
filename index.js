daafce1dcaf5a4bcb694d3a1724404b08302f871
Isabelle Yiu <isabelle.yiu.jobs@gmail.com>
2019-02-24 18:21:44 -0800

    Refactor psql code with sequelize

--- index.js
+++ index.js
@@ -1,32 +1,64 @@
 require('dotenv').config();
+
 const express = require('express');
 const bodyParser = require('body-parser');
-const db = require('./queries');
+
+const db = require('./models');
 
 const app = express();
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({extended: false}));
 
 
 app.get('/', (req, res) => {
   res.send('Welcome to my blogsite!');
 });
 
-// display all articels
-app.get('/articles', db.getArticles);
+// // display all articels
+app.get('/articles', (req, res) => {
+  db.Article.findAll().then(articles => res.json(articles));
+});
 
 // add new article
-app.post('/articles', db.addArticle);
+app.post('/articles', (req, res) => {
+  db.Article.create({
+    title: req.body.title,
+    author: req.body.author,
+    content: req.body.content
+  }).then(result => res.json(result));
+});
 
 // get one article by Id
-app.get('/articles/:id', db.getArticleById);
+app.get('/articles/:id', (req, res) => {
+  db.Article.findById(req.params.id).then(result => res.json(result));
+});
 
-// update article by Id
-app.put('/articles/:id', db.updateArticle);
+// // update article by Id
+app.put('/articles/:id', (req, res) => {
+  db.Article.update({
+    title: req.body.title,
+    author: req.body.author,
+    content: req.body.content
+  },
+  {
+    where: {
+      id: req.params.id
+    }
+  }).then(result => res.json(result));
+});
 
-// delete article by Id
-app.delete('/articles/:id', db.deleteArticle);
+// // delete article by Id
+app.delete('/articles/:id', (req, res) => {
+  db.Article.destroy({
+    where: {
+      id: req.params.id
+    }
+  }).then(result => res.json(result));
+});
 
-app.listen(3000, () => {
-  console.log('blog server is running');
+
+db.sequelize.sync().then(()=> {
+  app.listen(3000, () => {
+    console.log('blog server is running');
+  });
 });
